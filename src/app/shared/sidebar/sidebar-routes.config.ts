afe5b3ed6357dab5007801bb41cd48fdce63fc6c
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2018-09-03 14:42:46 +0530

    Revert "Revert "Initial commit""
    This reverts commit 0f781eb32a80d13a1fc0ac4b3765e0c6d4262971.

---
+++ src/app/shared/sidebar/sidebar-routes.config.ts
@@ -0,0 +1,8 @@
+import { RouteInfo } from './sidebar.metadata';
+
+export const ROUTES: RouteInfo[] = [
+
+    {
+        path: '/register', title: 'Register', icon: 'ft-edit', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
+    }
+];
