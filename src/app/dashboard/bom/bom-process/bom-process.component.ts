39bbdbee84280982bb40ed6a62bb0216e70e582c
Jatin Parmar <jatinparmar96@gmail.com>
2018-10-26 15:28:57 +0530

    Update bom process with form Data Service

--- src/app/dashboard/bom/bom-process/bom-process.component.ts
+++ src/app/dashboard/bom/bom-process/bom-process.component.ts
@@ -5,6 +5,7 @@ import { FormGroup } from '@angular/forms';
 import { FormlyFormOptions, FormlyFieldConfig, FieldType } from '@ngx-formly/core';
 import { NgOption } from '@ng-select/ng-select';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { FormDataService } from 'app/shared/services/form-data.service';
 
 @Component({
   // formly-app-example
@@ -15,16 +16,22 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 export class BomProcessComponent implements OnInit {
 
   closeResult: string;
+  current_step = 2;
   constructor(
     private router:Router,
-    private modalService: NgbModal
+    private modalService: NgbModal,
+    private formService:FormDataService
   )
-   { }
+  {
+
+  }
 
   ngOnInit() {
   }
 
-  toNext(){
+  toNext(bom){
+    this.formService.toNext(bom.value,this.current_step)
+    console.log(this.formService.getData());
     this.router.navigateByUrl('/dashboard/bom/byproduct');
   }
   toPrevious(){
