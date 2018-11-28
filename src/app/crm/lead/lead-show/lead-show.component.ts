450325da31acb3653d9377443fc2bfa5ff6a9bbb
bitmanity <bitmanity@gmail.com>
2018-11-28 14:47:01 +0530

    Angular 6 Commit

--- src/app/crm/lead/lead-show/lead-show.component.ts
+++ src/app/crm/lead/lead-show/lead-show.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lead-show.component.scss']
 })
 export class LeadShowComponent implements OnInit {
-
+  active= 'today';
   constructor() { }
 
   ngOnInit() {
